3f5193afdb29b74524c7cd4232ec6a03349453e3
thanhhuynh8989-stack <thanhhuynh8989@gmail.com>
2026-09-16 15:48:29 +0700

    Create apiEndpoints.js

---
+++ constants/apiEndpoints.js
@@ -0,0 +1,19 @@
+/
+
+API Endpoints Configuration
+*/
+
+export const API_ENDPOINTS = {
+GITHUB: {
+BASE_URL: 'https://api.github.com',
+EXAMS_DIR: 'exams',
+KEYS_DIR: 'keys'
+},
+GEMINI: {
+BASE_URL: 'https://generativelanguage.googleapis.com/v1beta',
+DEFAULT_MODEL: 'gemini-1.5-flash'
+},
+APPS_SCRIPT: {
+STORAGE_KEY: 'apps_script_web_app_url'
+}
+};
